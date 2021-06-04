4fb16be939a0d11ed1a23ba29de767f62ab85c97
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-03 22:28:09 -0400

    anbox-postmarketos-image: init at 7.1.2_r39-anbox

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1735,6 +1735,8 @@ in
 
   anbox = callPackage ../os-specific/linux/anbox { };
 
+  anbox-postmarketos-image = callPackage ../os-specific/linux/anbox/postmarketos-image.nix { };
+
   androidenv = callPackage ../development/mobile/androidenv {
     pkgs_i686 = pkgsi686Linux;
   };
